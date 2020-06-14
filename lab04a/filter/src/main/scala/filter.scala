8c61ff5339f42b2efd45214c5a5226f957554b0e
Marat Gumarov <ikey678@gmail.com>
2020-06-14 23:21:06 +0300

    add offset param

--- lab04a/filter/src/main/scala/filter.scala
+++ lab04a/filter/src/main/scala/filter.scala
@@ -20,7 +20,8 @@ object filter extends App {
 
   val kafkaParams = Map(
     "kafka.bootstrap.servers" -> "10.0.1.13:6667",
-    "subscribe" -> topic
+    "subscribe" -> topic,
+    "startingOffsets" -> s""" { "$topic": { "0": $offset } } """
   )
 
   val inputRaw = spark
