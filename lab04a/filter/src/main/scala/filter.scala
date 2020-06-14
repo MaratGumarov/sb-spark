7e8f745c374fe4cb1963701118083684ee4c0f1d
Marat Gumarov <ikey678@gmail.com>
2020-06-14 21:06:55 +0300

    make views and buy filters

--- lab04a/filter/src/main/scala/filter.scala
+++ lab04a/filter/src/main/scala/filter.scala
@@ -1,21 +1,46 @@
-import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
 
 object filter extends App{
-    val spark = SparkContext.getOrCreate()
+    val spark = SparkSession.builder().getOrCreate()
+    import spark.implicits._
+
+    val sc = spark.sparkContext
+
+    val topic = sc.getConf.get("spark.filter.topic_name")
+    val offset = sc.getConf.get("spark.filter.offset")
+    val target = sc.getConf.get("spark.filter.output_dir_prefix")
+
+    val kafkaParams = Map(
+        "kafka.bootstrap.servers" -> "10.0.1.13:6667",
+        "subscribe" -> topic
+    )
+
+    val inputRaw = spark
+      .read.format("kafka")
+      .options(kafkaParams)
+      .load
+
+    val rawJsons = inputRaw
+      .select(col("value").cast("String")).as[String]
+
+    val parsedInput = spark.read.json(rawJsons)
+      .withColumn("month_col", from_unixtime('timestamp/1000, "MM"))
+      .withColumn("day_col", from_unixtime('timestamp/1000, "DD"))
+
+    val buys = parsedInput.filter(col("event_type") === "buy")
+    buys.show(5, truncate = false)
+    buys.write
+      .partitionBy("month_col", "day_col")
+      .mode("overwrite")
+      .parquet(target + "/buy")
 
 
-    //spark-submit
-    // --conf spark.filter.topic_name=lab04_input_data
-    // --conf spark.filter.offset=earliest
-    // --conf spark.filter.output_dir_prefix=/user/name.surname/visits
-    // --class filter
-    // --packages
-    //      org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5
-    //      .target/scala-2.11/filter_2.11-1.0.jar
-    println("spark.filter.topic_name=" + spark.getConf.get("spark.filter.topic_name"))
-    println("filter.topic_name" + spark.getConf.get("filter.topic_name"))
-
-//    val kafkaParams = Map(
-//        "kafka.bootstrap.servers" -> "10.0.1.13:6667",
-//        "subscribe" -> "lab04_input_data"
+    val views = parsedInput.filter(col("event_type") === "view")
+    views.show(5, truncate = false)
+    views.write
+        .partitionBy("month_col", "day_col")
+        .mode("overwrite")
+        .parquet(target + "/view")
 }
